ca832410af612a457e52a33d1490f51e9c9c6732
adham90 <adham.eldeeb90@gmail.com>
2018-05-11 18:27:50 +0200

    hackerrank big sum

---
+++ big_sum.js
@@ -0,0 +1,44 @@
+//hackerrank https://www.hackerrank.com/challenges/a-very-big-sum/problem
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+function aVeryBigSum(n, ar) {
+   return ar.reduce((a, b) => a + b);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    let result = aVeryBigSum(n, ar);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
